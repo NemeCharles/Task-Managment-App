396233ad94c6f9942e5ac0ea3208cab322890b83
NemeCharles <106552372+NemeCharles@users.noreply.github.com>
2023-03-04 01:57:06 +0100

    created view task screen

---
+++ lib/screens/view_task_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ViewTask extends StatelessWidget {
+  const ViewTask({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(),
+    );
+  }
+}
